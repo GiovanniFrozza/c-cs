40bd969c9e1fc0807d3129d0f6d37b1b64f86b9a
giovanni <giovannifrozza470@gmail.com>
2020-11-20 15:20:13 -0300

    matriz

---
+++ aula010 matrizes/matrizes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] matriz = new char[2, 2] {
+                {'A', 'B'},
+                {'C', 'D'},
+            };
+
+            for (int linha = 0; linha < 2; linha++)
+            {
+                for(int coluna = 0; coluna < 2; coluna++)
+                {
+                    Console.Write("{0} |",matriz[linha, coluna]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
